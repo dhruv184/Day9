42ca20dc482141fe192d080c1d106e8df7142fb5
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2024-02-05 14:26:27 -0500

    Added project

---
+++ Example1Day9/Day9/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Day9;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ Example1Day9/Day9/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Day9
+{
+	public class User
+	{
+		public string Id { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public string Email { get; set; }
+    }
+}
+
